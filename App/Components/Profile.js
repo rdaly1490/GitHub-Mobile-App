ac6fc4590944775a34b0882630baa293015bdcc1
Rob <rdaly1490@gmail.com>
2015-08-02 19:26:40 -0400

    profile component complete

--- App/Components/Profile.js
+++ App/Components/Profile.js
@@ -9,10 +9,35 @@ var {
 } = React
 
 class Profile extends React.Component{
+	//function to format items in the array when rendered on page
+	getRowTitle(user, item){
+		item = (item === "public_repos") ? item.replace("_"," ") : item;
+		return item[0] ? item[0].toUpperCase()+item.slice(1) : item;
+	}
 	render(){
+		//this.props.userInfo comes from dash, assigning to variable
+		var userInfo = this.props.userInfo;
+		//Array of info I want to display
+		var topicArray = ["company","location","followers","following","email","bio","public_repos"];
+		//mapping content: if exists on user page will display otherwise empty view
+		//uses array index as key for react
+		var content = topicArray.map((item, index) => {
+			//[ ] b/c array, stop thinking it's an object!
+			if(!userInfo[item]) {
+				return <View key={index}></View>
+			}else {
+				return(
+					<View key={index}>
+						<Text style={styles.rowTitle}>{this.getRowTitle(userInfo, item)}</Text>
+						<Text style={styles.rowContent}>{userInfo[item]}</Text>
+					</View>
+				);
+			}
+		});
 		return(
 			<ScrollView style={styles.container}>
 				<Badge userInfo={this.props.userInfo} />
+				{content}
 			</ScrollView>
 		);
 	}
@@ -32,10 +57,15 @@ var styles = StyleSheet.create({
   },
   rowTitle: {
     color: '#48BBEC',
+    marginTop: 12,
+    marginBottom: 4,
     fontSize: 16
+    // alignSelf: 'center'
   },
   rowContent: {
-    fontSize: 19
+    fontSize: 19,
+    marginBottom: 8
+    // alignSelf: 'center'
   }
 });
 
